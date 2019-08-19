7262ff56517214459cd94e16e07e092354512bab
vasunangalia <vasunangalia789@gmail.com>
2019-08-19 13:25:38 +0530

    first deploy 3

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.use("/", require("./routes/api/index"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started`));
